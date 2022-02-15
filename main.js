2dbdf3fd194c8930de5a5bea089d8fb4c097d5db
jandershock <jordan.andershock@gmail.com>
2022-02-15 09:41:17 -0600

    All functions added and working

---
+++ main.js
@@ -0,0 +1,72 @@
+//Get Paper
+const getPaper = () => {
+    console.log('Get a piece of A3 paper');
+}
+
+//Fold paper
+const foldPaper = () => {
+    console.log('Fold it in half');
+}
+
+//Clean fountain pen
+const cleanFountainPen = () => {
+    console.log('Now clean your fountain pen');
+}
+
+//Fill fountain pen with specified ink color
+const fillFountainPen = () => {
+    console.log('Fill your fountain pen with your favorite ink');
+}
+
+//Write letter
+const writeLetter = () => {
+    console.log('Write your letter');
+}
+
+//Fold letter into envelope to be addressed
+const foldLetter = () => {
+    console.log('Fold the letter and place in envelope');
+}
+
+//Melt wax specified wax colors
+const meltWax = () => {
+    console.log('Melt wax colors you want for the seal');
+}
+
+//Pour wax onto letter
+const pourWax = () => {
+    console.log('Pour wax onto the letter');
+}
+
+//Press seal into wax
+const pressSeal = () => {
+    console.log('Press the seal into wax');
+}
+
+//Address envelope
+const addressEnvelope = () => {
+    console.log('Address the envelope');
+}
+
+//Add stamp to envelope
+const addStamp = () => {
+    console.log('Add a stamp to the envelope');
+}
+
+//Mail letter
+const mailLetter = () => {
+    console.log('Take the letter to the post office or place in your mail box and mail it');
+}
+
+getPaper();
+foldPaper();
+cleanFountainPen();
+fillFountainPen();
+writeLetter();
+foldLetter();
+meltWax();
+pourWax();
+pressSeal();
+addressEnvelope();
+addStamp();
+mailLetter();
